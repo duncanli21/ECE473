c6214809b7cd03840be85115c0f48715d52f4df2
Liam Duncan <duncanli@oregonstate.edu>
2020-10-01 15:40:54 -0700

    Lab1 Final

--- Lab1/lab1_code.c
+++ Lab1/lab1_code.c
@@ -16,10 +16,28 @@
 // function can be implemented at the same time. Expects active low pushbutton on 
 // Port D bit zero. Debounce time is determined by external loop delay times 12. 
 //*******************************************************************************
-int8_t debounce_switch() {
-  static uint16_t state = 0; //holds present state
-  state = (state << 1) | (! bit_is_clear(PIND, 0)) | 0xE000;
-  if (state == 0xF000) return 1;
+int8_t debounce_switch0() {
+  static uint16_t state0 = 0; //holds present state
+  
+  state0 = (state0 << 1) | (! bit_is_clear(PIND, 0)) | 0xE000;
+  if (state0 == 0xF000) return 1;
+  return 0;
+}
+
+
+//*******************************************************************************
+//                            debounce_switch                                  
+// Adapted from Ganssel's "Guide to Debouncing"            
+// Checks the state of pushbutton S0 It shifts in ones till the button is pushed. 
+// Function returns a 1 only once per debounced button push so a debounce and toggle 
+// function can be implemented at the same time. Expects active low pushbutton on 
+// Port D bit zero. Debounce time is determined by external loop delay times 12. 
+//*******************************************************************************
+int8_t debounce_switch1() {
+  static uint16_t state1 = 0; //holds present state
+  
+  state1 = (state1 << 1) | (! bit_is_clear(PIND, 1)) | 0xE000;
+  if (state1 == 0xF000) return 1;
   return 0;
 }
 
@@ -30,8 +48,11 @@ int8_t debounce_switch() {
 int main()
 {
 DDRB = 0xFF;  //set port B to all outputs
+DDRD = 0x00;  // set port D to all inputs
 while(1){     //do forever
- if(debounce_switch()) {PORTB++;}  //if switch true for 12 passes, increment port B
+ if(debounce_switch0() == 1) {PORTB++;}  //if switch true for 12 passes, increment port B
+ if(debounce_switch1() == 1) {PORTB--;}  //if switch true for 12 passes, decrement port B
+
   _delay_ms(2);                    //keep in loop to debounce 24ms
   } //while 
 } //main
