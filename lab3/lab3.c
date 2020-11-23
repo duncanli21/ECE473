a2efad159384dfd754388c3a9389d664c6a3eb59
Liam Duncan <duncanli@oregonstate.edu>
2020-11-23 15:38:18 -0800

    More Files

--- lab3/lab3.c
+++ lab3/lab3.c
@@ -17,6 +17,21 @@
 //
 // PORTD bit 2 goes to regclk
 
+
+#include <avr/io.h> 
+#include <avr/interrupt.h>
+#include <util/delay.h> 
+#include <stddef.h> 
+
+// Define the PORTB Connections that we will be using
+#define SEL_0 0x00
+#define SEL_1 0x10
+#define SEL_2 0x20
+#define SEL_3 0x40
+#define PWM 0x80 
+
+
+
 //holds data to be sent to the segments. logic zero turns segment on
 uint8_t segment_data[5]; 
 
@@ -37,18 +52,35 @@ uint8_t dec_to_7seg[12] =  {
        0b11111111, // Clear 
 };
 
-// Function to configure timer counters 
-void initRegisters()
-{
-     //timer counter 0 setup, running off i/o clock
-     TIMSK |= (1<<TOIE0);             //enable interrupts
-     TCCR0 |= (1<<CS02) | (1<<CS00);  //normal mode, prescale by 128
 
-     // Configure SPI 
-     // clk low on idle, leading edge sample, Master mode
-     SPCR = (1<<SPE) | (1<<MSTR) | (0<<CPHA) | (0<<CPOL);
+uint8_t output = 0; 
+uint8_t countBy2 = 0; 
+uint8_t countBy4 = 0;
+uint16_t counter = 0; 
+
+uint8_t prevENC = 0x00;
+
+void add(){
+	if(countBy2 && !countBy4)
+		counter += 2; 
+	else if(countBy4 && !countBy2)
+		counter += 4;  
+	else if(!countBy2 && !countBy4)
+		counter += 1; 
+
+} 
+
+
+void subtract(){ 
+        if(countBy2 && !countBy4)
+                counter -= 2;
+        else if(countBy4 && !countBy2)
+                counter -= 4;
+        else if(!countBy2 && !countBy4)
+                counter -= 1;
 
 }
+
 
 //******************************************************************************
 //                            chk_buttons
@@ -70,6 +102,116 @@ uint8_t chk_buttons(uint8_t button) {
         return 0;  // return a 0 if the button was not pressed
 }
 //******************************************************************************
+
+
+
+ISR(TIMER0_OVF_vect)
+{
+	  int i = 0;
+
+	  uint8_t static sum = 0; 
+
+  //make PORTA an input port with pullups 
+          DDRA = 0x00; // make port A an input so that it can read buttons 
+          PORTA = 0xFF;
+
+ //enable tristate buffer for pushbutton switches
+          PORTB = 0b11110000;
+
+          _delay_us(5); 
+
+  //now check each button and increment the count as needed
+        for(i=0; i<2; i++){
+
+                if(chk_buttons(i)){
+			if(i == 0) 
+				countBy2 ^= 1;
+			else if(i == 1)
+				countBy4 ^= 1; 
+
+		} 
+          }
+	
+  	if((countBy2 == 1) && (countBy4 != 1))
+	       output = 0b00000001; 
+
+	if((countBy4 == 1) && (countBy2 != 1))
+		output = 0x02;
+
+	if((countBy2 == 1) && (countBy4 == 1))
+		output = 0x03; 
+
+	if((countBy2 == 0) && (countBy4 == 0))
+		output = 0x00; 
+	
+	
+
+	PORTB = 0xFF; // disable the tri state buffer 
+
+	DDRA = 0xFF; // make porta an output again 
+
+
+	// push the button state to the bargraph 
+	SPDR = output; 
+	
+	while(bit_is_clear(SPSR, SPIF)){};
+	prevENC = SPDR; 
+
+
+        PORTE &= ~(0x40); // Enable CLK 
+        PORTE |= (0x80); // disable LD/SH
+
+
+                // latch clk to push bar graph 
+	PORTD |= 0x04; 
+	PORTD &= ~0x04; 
+
+        PORTE &= ~(0x80); // Enable CLK
+        PORTE |= (0x40); // disable LD/SH
+
+
+
+	uint8_t static LastENCL = 0; 
+	uint8_t static LastENCR = 0; 
+	uint8_t static ENCL = 0; 
+	uint8_t static ENCR = 0; 
+	
+	LastENCL = ENCL; 
+	LastENCR = ENCR; 
+
+	ENCL = (prevENC & 0b00000011); 
+	ENCR = ((prevENC & 0b00001100) >> 2);	
+		
+	if(ENCL != LastENCL){
+		if((LastENCL == 0x01) && (ENCL == 0x03))
+			add(); 
+		if((LastENCL == 0x02) && (ENCL == 0x03))
+			subtract(); 
+	} 
+
+	if(ENCR != LastENCR){
+                if((LastENCR == 0x01) && (ENCR == 0x03))
+                        add();
+                if((LastENCR == 0x02) && (ENCR == 0x03))
+                        subtract();
+        }
+
+	
+
+}
+// Function to configure timer counters 
+void initRegisters()
+{
+     //timer counter 0 setup, running off i/o clock
+     TIMSK |= (1<<TOIE0);             //enable interrupts
+     TCCR0 |= (1<<CS02) | (1<<CS00);  //normal mode, prescale by 128
+
+     // Configure SPI 
+     // clk low on idle, leading edge sample, Master mode
+     SPCR = (1<<SPE) | (1<<MSTR) | (0<<CPHA) | (0<<CPOL);
+
+}
+
 
 
 //***********************************************************************************
@@ -115,13 +257,59 @@ uint8_t main()
 	DDRD = 0xFF; // Set Port D as all outputs 
 	DDRE |= 0xFF; // set all bits as an output on port E 
 
-	PORTE |= 0x40; // disable CLK_INH 
-        PORTE &= ~(0x80) // enable SH/LD_n 
+	PORTE |= 0x80; // disable CLK_INH 
+        PORTE &= ~(0x40); // enable SH/LD_n 
+	
+	initRegisters(); 	
 
         sei();   // enable interrupts 
         
-         
+        PORTB = 0xF0; 
 
 
+	while(1){
+
+	_delay_ms(1);  
+
+	if(counter > 1023) 
+		counter =0; 
+
+	if (counter > 10000)
+		counter = 1023;
+
+        
+
+	segsum(counter); 
+
+
+ 	   //make PORTA an output
+           DDRA = 0xFF;
+	   PORTB = 0x00; 
+
+           _delay_us(100); 
+
+	  //send 7 segment code to LED segments
+          PORTA = segment_data[0];
+          PORTB = SEL_0;
+          _delay_ms(1);
+          PORTA = segment_data[1];
+          PORTB = SEL_1;
+          _delay_ms(1);
+          PORTA = segment_data[2];
+          PORTB = SEL_2;
+          _delay_ms(1);
+          PORTA = segment_data[3];
+          PORTB = 0x30;
+          _delay_ms(1);
+          PORTA = segment_data[4];
+          PORTB = 0x40;
+
+          _delay_us(100);
+
+
+	
+
+
+	}
 
 }// main 
